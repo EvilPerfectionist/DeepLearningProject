d082d9e7a78c752ce3d494d53d8a8b15c334d4d5
EvilPerfectionist <17862701097@163.com>
2020-04-29 13:39:25 +0200

    1

--- project.py
+++ project.py
@@ -2,8 +2,9 @@ import numpy as np
 import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import cv2
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture('LessonsAtHome.mp4')
+cap = cv2.VideoCapture('/home/leon/DeepLearning/Project/LessonsAtHome.mp4')
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -11,6 +12,12 @@ while(cap.isOpened()):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('frame',gray)
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        histr = cv.calcHist([frame],[i],None,[256],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.show()
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
